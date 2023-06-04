6a29a6fe1ef1c9a832ded35b8562d9837e7a7b3b
m1guelten <footballmiguelten@gmail.com>
2023-06-04 17:55:28 +0300

    delete array play & use ternary operators

--- js/game.js
+++ js/game.js
@@ -53,17 +53,6 @@ const ST_DELTA = 32;
 const ST_DOZVIL = false;
 const MAX_ARREY_FISHKA = 1 + NUMBER_FISHKA * 3;
 
-const play = [
-  'White',
-  'Black',
-  'fishka_W',
-  'fishka_B',
-  'koordW',
-  'koordB',
-  'checkersWhite',
-  'checkersBlack',
-];
-
 const gameLet = {
   player: 0,
   step: 0,
@@ -71,7 +60,6 @@ const gameLet = {
   move: 0,
   dir: -1,
   space: true,
-  fishka: 'fishka_W',
 };
 
 const fishka_B = new Array(MAX_ARREY_FISHKA);
@@ -111,7 +99,6 @@ for (let i = 0; i <= FULL_CIRCLE; i++) {
   else koordB[i] = { x: ST_X + BOX * (FULL_CIRCLE - i), y: ST_Y };
   i < BIAS ? (koordW[i + BIAS] = koordB[i]) : (koordW[i - BIAS] = koordB[i]);
 }
-
 koordW[FULL_CIRCLE] = koordW[0];
 for (let i = 1; i < MAX_NUMBER_MOVES - FULL_CIRCLE; i++) {
   koordB[i + FULL_CIRCLE] = { x: ST_X + BOX * i, y: ST_Y + BOX * i };
@@ -305,20 +292,19 @@ const run = (colorA, colorB, dirN = -1) => {
 };
 
 const gameOver = () => {
-  next();
   styleText('white', '100px Arial');
-  ctx.fillText(gameLet.play[player], ST_TEXT_X, ST_TEXT_Y);
+  ctx.fillText(gameLet.player === 0 ? 'Black' : 'White', ST_TEXT_X, ST_TEXT_Y);
   ctx.fillText('WINNER', ST_WIN_X, ST_WIN_Y);
   document.removeEventListener('keydown', direction);
 };
 
 const painting = (player) => {
-  const noPlayer = player + Math.pow(-1, player);
-  const checker = play[player + 6];
-  const koord = eval(play[player + 4]);
-  gameLet.fishka = eval(play[player + 2]);
+  const koord = player === 0 ? koordW : koordB;
+  const fishka = player === 0 ? fishka_W : fishka_B;
+
   styleText('white', '100px Arial');
-  ctx.fillText(play[player], ST_TEXT_X, ST_TEXT_Y);
+  ctx.fillText(player === 0 ? 'White' : 'Black', ST_TEXT_X, ST_TEXT_Y);
+
   for (let i = 1; i <= fishka_W[0]; i++) {
     ctx.drawImage(
       sprites.get('checkersWhite'),
@@ -328,18 +314,18 @@ const painting = (player) => {
   }
   for (let i = 1; i <= fishka_B[0]; i++) {
     ctx.drawImage(
-      sprites.get('checkersWhite'),
-      koordW[fishka_W[i]].x,
-      koordW[fishka_W[i]].y
+      sprites.get('checkersBlack'),
+      koordB[fishka_B[i]].x,
+      koordB[fishka_B[i]].y
     );
   }
-  for (let i = 1; i <= gameLet.fishka[0]; i++) {
-    if (gameLet.fishka[i + DOZVIL_FISHKA] === true && gameLet.space === false) {
-      styleText(play[noPlayer], '20px Arial');
+  for (let i = 1; i <= fishka[0]; i++) {
+    if (fishka[i + DOZVIL_FISHKA] === true && gameLet.space === false) {
+      styleText(player === 0 ? 'Black' : 'White', '20px Arial');
       ctx.fillText(
         i,
-        koord[gameLet.fishka[i]].x + ST_NUM_X,
-        koord[gameLet.fishka[i]].y + ST_NUM_Y
+        koord[fishka[i]].x + ST_NUM_X,
+        koord[fishka[i]].y + ST_NUM_Y
       );
     }
   }
@@ -347,12 +333,26 @@ const painting = (player) => {
 
 const controlDiceSix = () => {
   if (gameLet.step !== 6) {
-    if (gameLet.fishka[0] !== 0) playChange(run);
+    if (gameLet.player === 0 ? fishka_W[0] : fishka_B[0] !== 0) playChange(run);
     else next();
   } else playChange(newFishka);
 };
 
 function drawGame() {
+  if ((gameLet.player === 0 ? fishka_W[0] : fishka_B[0]) === 0) {
+    if (gameLet.dir === 0) {
+      drive();
+      controlDiceSix();
+    }
+  }
+  if ((gameLet.player === 0 ? fishka_W[0] : fishka_B[0]) !== 0) {
+    playChange(delta);
+    if (gameLet.dir === 0 && gameLet.space === true) {
+      drive();
+      controlDiceSix();
+    }
+    if (gameLet.dir > 0 && gameLet.space === false) controlDiceSix();
+  }
   ctx.drawImage(sprites.get('ground'), 0, 0);
   ctx.drawImage(
     sprites.get(`dices${gameLet.step}`),
@@ -365,22 +365,6 @@ function drawGame() {
   )
     gameOver();
   painting(gameLet.player);
-
-  if (gameLet.fishka[0] === 0) {
-    if (gameLet.dir === 0) {
-      drive();
-      controlDiceSix();
-    }
-  }
-  if (gameLet.fishka[0] !== 0) {
-    playChange(delta);
-    if (gameLet.dir === 0 && gameLet.space === true) {
-      drive();
-      controlDiceSix();
-    }
-
-    if (gameLet.dir > 0 && gameLet.space === false) controlDiceSix();
-  }
 }
 
 drawGame();
